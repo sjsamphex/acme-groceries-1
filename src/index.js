4ebd2ec7dec4cb9728799d0efb3c86482f7976f7
sjsamphex <sjsamphex@gmail.com>
2020-12-12 08:14:19 -0800

    attempt at setting up dispatch of groceries

--- src/index.js
+++ src/index.js
@@ -1,34 +1,48 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { Provider, connect } from 'react-redux';
-import store from './store';
-// import axios from './axios';
+import store, { LOAD_GROCERIES } from './store';
+import axios from 'axios';
 
 class _App extends Component {
   constructor(props) {
     super(props);
     //should this do state equals getState from store?
   }
+  // componentDidMount() {
+  //   //do a dispatch somehow? or receive props data
+  // }
   render() {
+    console.log('app sees this', this.props.groceries);
     return (
       <div>
         <h1>Acme Groceries</h1>
-        {this.props.groceries}
+        {/* {this.props.groceries.map((groc) => (
+          <li>{groc.name}</li>
+        ))} */}
       </div>
     );
   }
 }
 
-const mapStateToProps = () => {
-  // const grocerydata = (axios()).data;
+const mapStateToProps = async () => {
+  const grocerydata = (await axios.get('/api/groceries')).data;
+  console.log('mapstatetoprops sees this: ', grocerydata);
   return {
-    groceries: 5,
+    groceries: grocerydata,
   };
 };
 //will get passed into the provider i think
 
 const mapDispatchToProps = () => {
-  return {};
+  return {
+    loadGroceries() {
+      return {
+        type: LOAD_GROCERIES,
+        store,
+      };
+    },
+  };
 };
 
 const App = connect(mapStateToProps, mapDispatchToProps)(_App);
--- src/store.js
+++ src/store.js
@@ -4,6 +4,17 @@ const state = {
   groceries: 5,
 };
 
+//action constant
+export const LOAD_GROCERIES = 'load groceries';
+
+//action creators
+const loadGroceries = (groceries) => {
+  return {
+    type: LOAD_GROCERIES,
+    state: groceries,
+  };
+};
+
 const store = createStore((initialState = state, action) => {
   if (action === null) {
   }
