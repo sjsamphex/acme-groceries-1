782d983d25b4ac3b6be7a46504aa3bb24adfb653
sjsamphex <sjsamphex@gmail.com>
2020-12-14 14:45:58 -0800

    nav bar selection status

--- src/Nav.js
+++ src/Nav.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const Nav = ({ groceries }) => {
+const Nav = ({ groceries, view }) => {
   const needGroc = groceries.filter((grocery) => !grocery.purchased);
   const purchasedGroc = groceries.filter((grocery) => grocery.purchased);
   return (
     <nav>
-      <a href="#">All ({groceries.length})</a>
-      <a href="#needs">Needs ({needGroc.length})</a>
-      <a href="#purchased">Purchased ({purchasedGroc.length})</a>
+      <a href="#" className={view === '' ? 'selected' : ''}>
+        All ({groceries.length})
+      </a>
+      <a href="#needs" className={view === 'needs' ? 'selected' : ''}>
+        Needs ({needGroc.length})
+      </a>
+      <a href="#purchased" className={view === 'purchased' ? 'selected' : ''}>
+        Purchased ({purchasedGroc.length})
+      </a>
     </nav>
   );
 };
